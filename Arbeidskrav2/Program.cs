94f29bfdcffa06eacd998676b39deb1e06f590bc
Aleksander Aasen <alekssaasen@gmail.com>
2026-03-18 23:00:19 +0100

    Added MarketplaceApp.cs, ready to implement UI

--- Arbeidskrav2/Program.cs
+++ Arbeidskrav2/Program.cs
@@ -6,37 +6,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Marketplace M = new Marketplace();
-
-        Console.WriteLine("MarketPlace test");
-        M.Register("Alek", "123");
-        M.Login("Alek", "123");
-        M.CreateListing("Sofa", "Nice sofa!", Category.FurnitureAndHome, Condition.LikeNew, 5000.00m);
-        M.Logout();
-
-        M.Register("Zoe", "123");
-        M.Login("Zoe", "123");
-        M.CreateListing("Bed", "Good to sit in!", Category.FurnitureAndHome, Condition.LikeNew, 5000.00m);
-        Console.WriteLine("Created Listing");
         
-        var listings = M.GetAllAvailableListings();
-        var firstListing = listings[0];
-        
-        var foundListing = M.SearchListings("Sofa");
-        Console.WriteLine($"Found listing! {foundListing.Count}");
-        M.PurchaseListing(firstListing);
-        Console.WriteLine("Purchase complete");
-        
-        // Made _users public for testing
-        /*
-        var userOne = M._users[0];
-        var firstPurchase = userOne.Transactions[0];
-        M.LeaveReview(firstPurchase,6, "Nice woman!");
-        Console.WriteLine($"Seller has {firstPurchase.Seller.Reviews.Count} reviews");
-
-        Console.WriteLine($"Average Rating: {userOne.GetAvgRating()}");
-        */
-        
-
     }
 }
---
+++ Arbeidskrav2/UI/MarketplaceApp.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace Arbeidskrav2.UI;
+
+public class MarketplaceApp
+{
+    private Marketplace Marketplace;
+    private string? userInput;
+
+    public MarketplaceApp(Marketplace marketplace)
+    {
+        Marketplace = marketplace;
+    }
+    
+    public void Run()
+    {
+        
+    }
+
+    public void StartMenu()
+    {
+        
+    }
+}
